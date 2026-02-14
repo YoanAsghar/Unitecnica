9feb761f78e1a2da5538e6f274aa1cab78337a96
Yoan <164660373+YoanAsghar@users.noreply.github.com>
2026-02-13 19:20:52 -0500

    Add files via upload

---
+++ JavaSript/Taller1/variable.js
@@ -0,0 +1,18 @@
+// Estudiante: Tu Nombre y Tus Apellidos
+// Grupo: Número del grupo
+// Curso: JavaScript Básico
+// Clase: Definición de variables y constantes
+// Crea otro archivo llamado constantes.js
+// Vamos a realizar un código que imprima por consola “Hola, mi nombre es ___ y tengo ___ años”, los campos deben ser rellenados con una constante que tenga el dato a imprimir.
+// Sube tus cambios al repositorio de github con el siguiente commit: “taller 1 javascript basico - aqui tu nombre completo”
+// Pega la url del repositorio aquí y sube el archivo al classroom: url_repositorio_github
+
+let nombre = "Joan Santiago Velasquez Arias";
+let grupo = 1192;
+let curso = "JavaScript";
+
+
+const name = "Santiago Joan Arias Velasquez";
+const age = 21;
+
+console.log(`Hola, mi nombre es ${name} y tengo ${age} años`);
---
+++ JavaSript/Taller2/main.js
@@ -0,0 +1,82 @@
+// Parte 1 – Declaración de variables (40 minutos)
+// 1. Declara una variable con tu nombre
+// 2. Declara una variable con tu edad
+// 3. Declara una constante con tu país
+// 4. Imprime todas en consola
+
+
+let myName = "Joan Santiago Velasquez Arias";
+let myAge = 21;
+const myCountry = "Colombia";
+
+console.log(`Mi nombre es ${myName}, tengo ${myAge} años y naci en ${myCountry}`);
+
+// Parte 2 – Tipos de datos (50 minutos)
+// Crea variables para:
+// - Un número decimal
+// - Un mensaje de texto
+// - Un valor verdadero
+// - Una variable sin valor
+// - Un valor null
+
+let float = 3.1416;
+let varchar = "Hello world";
+let bool = true;
+let noValue;
+let myNull = null;
+
+// Parte 3 – Verificar tipos con typeof (40 minutos)
+// Usa typeof para verificar:
+// typeof edad
+// typeof mensaje
+// typeof activo
+// typeof valor
+
+console.log(typeof(myAge));
+console.log(typeof(varchar));
+console.log(typeof(float));
+console.log(typeof(bool));
+console.log(typeof(myNull));
+
+// Parte 4 – Pequeños retos (40 minutos)
+// 1. Guarda el precio de un producto
+// 2. Guarda la cantidad comprada
+// 3. Calcula el total
+// 4. Muestra un mensaje como:
+// "El total a pagar es: $___"
+
+let amountOfProduct = 4;
+let priceOfProduct = 20;
+
+console.log(`El total a pagar es : ${amountOfProduct * priceOfProduct}`);
+
+// Parte 5 – Objeto básico (40 minutos)
+// Crea un objeto persona con:
+// nombre
+// edad
+// ciudad
+// Imprime cada propiedad
+
+let myObject = {
+    objectName : myName,
+    objectAge: myAge,
+    objectCountry: myCountry
+}
+
+console.log(`Mi nombre es ${myObject.objectName}, tengo ${myObject.objectAge} y soy de ${myObject.objectCountry}`);
+
+// Reto final (30 minutos)
+// Crea un programa que guarde:
+// nombre del estudiante
+// nota1
+// nota2
+// nota3
+// Calcula el promedio e imprime:
+// "El estudiante ___ tiene promedio ___"
+
+let nota1 = 3.7;
+let nota2 = 2.5;
+let nota3 = 5;
+
+let notaFinal = (nota1 + nota2 + nota3) / 3;
+console.log(notaFinal);
